f3499feb6d7777aa435693bce961fcc7c01f0e1f
angel m <angelmartinez@Angels-MBP.lan1>
2019-07-22 14:08:47 -0400

    adding html

---
+++ assets/javascript.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyArrl9oJGr0Duz8o_6B_7_IEdrz0v1u-8g",
+    authDomain: "homework-7-e0321.firebaseapp.com",
+    databaseURL: "https://homework-7-e0321.firebaseio.com",
+    projectId: "homework-7-e0321",
+    storageBucket: "",
+    messagingSenderId: "478530307735",
+    appId: "1:478530307735:web:157705b1456046a0"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
+
+  var database = firebase.database(); 
